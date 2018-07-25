028b81edc582608ada47b1fc6444ea8b06d333cd
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-25 14:10:53 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,6 @@ end
 post '/songs' do
   @song = Song.create(:name => params[:song_name])
   @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-  binding.pry
   @song.genres << Genre.find_or_create_by(:name => params["genre_name"])
   @song.save
   flash[:message] = "Successfully created song."
